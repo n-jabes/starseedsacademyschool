a4b4afdd4996e43544c8901e78c4cc13f21519c3
n-jabes <nshutij7@gmail.com>
2024-03-29 22:22:37 +0200

    finished website

---
+++ js/script.js
@@ -0,0 +1,124 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+  navbar.classList.toggle('active');
+  loginForm.classList.remove('active');
+};
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+  loginForm.classList.toggle('active');
+  navbar.classList.remove('active');
+};
+
+window.onscroll = () => {
+  navbar.classList.remove('active');
+  loginForm.classList.remove('active');
+};
+
+let homeNext = document.querySelector('.home-next');
+let homePrev = document.querySelector('.home-prev');
+
+homeNext.addEventListener('click', () => {
+  let items = document.querySelectorAll('.item');
+  document.querySelector('.slide').appendChild(items[0]);
+});
+
+homePrev.addEventListener('click', () => {
+  let items = document.querySelectorAll('.item');
+  document.querySelector('.slide').prepend(items[items.length - 1]);
+});
+
+var reviewSwiper = new Swiper('.review-slider', {
+  spaceBetween: 20,
+  centeredSlides: true,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var staffSwiper = new Swiper('.staff-slider', {
+  spaceBetween: 20,
+  centeredSlides: true,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+var newsSwiper = new Swiper('.news-slider', {
+  spaceBetween: 20,
+  centeredSlides: true,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
